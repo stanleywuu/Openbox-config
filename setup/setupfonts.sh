f2a6ddd91015039bee6f22d8f9ab5834175186e0
stanleywuu <stan.wu@alum.utoronto.ca>
2020-09-14 21:31:45 -0400

    updated set up script to alias terminator and qutebrowser

---
+++ setup/setupfonts.sh
@@ -0,0 +1,6 @@
+echo "Downloading fonts from hbin"
+# Courtesy of https://github.com/hbin/top-programming-fonts
+curl -L https://github.com/hbin/top-programming-fonts/raw/master/install.sh | bash
+
+echo "Time to set up the fonts we have just downloaded"
+obconf &
--- setup/setupscript.sh
+++ setup/setupscript.sh
@@ -10,18 +10,21 @@ sudo apt-get install snapd
 sudo apt-get install filezilla
 sudo apt-get install conky
 sudo apt-get install terminator
-
 sudo apt-get install lxappearance
 
 #browsers
 sudo apt-get install qutebrowser
 
 #set aliases
-alias configterm='terminator -l Config'
-alias term=terminator
-alias browser=qutebrowser
+#courtesy of https://stackoverflow.com/a/3557165
+FILE="~/.bashrc"
+LINE='alias term=terminator'
+sudo grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+LINE='alias browser=qutebrowser'
+sudo grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+LINE = "alias configterm='terminator -l Config"
+sudo grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 
 sh devSetup.sh
-
 
 lxappearance
