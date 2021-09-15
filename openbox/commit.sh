3cebef575127ce02980e914c3a39fcc759e728ca
Icebear <stan.wu@alum.utoronto.ca>
2021-09-15 19:43:37 -0400

    missing config

--- openbox/commit.sh
+++ openbox/commit.sh
@@ -1,6 +1,6 @@
 read -p "Do you want to commit your changes to source control?" yn
 case $yn in
-	[Yy]* ) sh ~/source/Openbox-config/copyFromConfig.sh; break;
-	[Nn]* ) exit;;
-	* ) exit;;
+	([Yy]* ) sh ~/source/Openbox-config/copyFromConfig.sh; break;
+	([Nn]* ) exit;;
+	(* ) exit;;
 esac
--- setup/vimsetup.sh
+++ setup/vimsetup.sh
@@ -1,2 +1,4 @@
 git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
