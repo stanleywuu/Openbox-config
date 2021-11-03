402d19e89000bf3c423a4bb9528cfc6cfca957f8
Icebear <stan.wu@alum.utoronto.ca>
2021-11-03 10:11:32 -0400

    added docker install to devSetup

--- setup/internal/devSetup.sh
+++ setup/internal/devSetup.sh
@@ -5,3 +5,7 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code
+
+sudo apt-get install docker-compose
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
